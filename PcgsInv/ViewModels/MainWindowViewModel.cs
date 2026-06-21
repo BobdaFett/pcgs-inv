51f1eb8c650cbce46241fdf5b5f6312966eec3f3
alphafetus <lvas463@gmail.com>
2026-06-21 00:47:18 -0400

    chore: commit missing files

---
+++ PcgsInv/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using PcgsApi;
+using PcgsInv.Models;
+using ReactiveUI;
+using ReactiveUI.SourceGenerators;
+
+namespace PcgsInv.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase {
+    [Reactive] private CoinCollection _collection;
+    [Reactive] private PcgsCoin? _selectedCoin;
+
+    public MainWindowViewModel() {
+        Collection = new CoinCollection();
+        _selectedCoin = null;
+
+        Collection.PropertyChanged += (s, e) => {
+            Debug.WriteLine($"Collection raised event: {e.PropertyName}");
+        };
+        
+        this.WhenAnyValue(x => x.Collection.TotalValue)
+            .Subscribe(x => Debug.WriteLine($"Total value was recomputed: {x}"));
+    }
+
+    [ReactiveCommand]
+    private void CreateTestingCoin() {
+        Collection.Coins.Add(new PcgsCoin());
+    }
+}
---
+++ PcgsInv/ViewModels/ViewModelBase.cs
@@ -0,0 +1,5 @@
+﻿using ReactiveUI;
+
+namespace PcgsInv.ViewModels;
+
+public abstract class ViewModelBase : ReactiveObject { }
